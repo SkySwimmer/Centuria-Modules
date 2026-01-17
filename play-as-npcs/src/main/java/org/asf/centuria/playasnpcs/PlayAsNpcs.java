b5fb34df0fb346140c80d7fac88273ad396831a0
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2026-01-17 16:28:04 +0100

    Port to string based def ids

--- play-as-npcs/src/main/java/org/asf/centuria/playasnpcs/PlayAsNpcs.java
+++ play-as-npcs/src/main/java/org/asf/centuria/playasnpcs/PlayAsNpcs.java
@@ -88,7 +88,7 @@ public class PlayAsNpcs implements ICenturiaModule {
 
 			// Save avatar to inventory
 			Player plr = event.getAccount().getOnlinePlayerInstance();
-			JsonArray items = plr.account.getPlayerInventory().getItem("avatars").getAsJsonArray();
+			JsonArray items = plr.account.getSaveSpecificInventory().getItem("avatars").getAsJsonArray();
 			JsonObject ts = new JsonObject();
 			ts.addProperty("ts", System.currentTimeMillis());
 			JsonObject nm = new JsonObject();
@@ -109,7 +109,7 @@ public class PlayAsNpcs implements ICenturiaModule {
 			items.add(avatar);
 
 			// Save
-			event.getAccount().getPlayerInventory().setItem("avatars", items);
+			event.getAccount().getSaveSpecificInventory().setItem("avatars", items);
 			InventoryItemPacket pk = new InventoryItemPacket();
 			pk.item = items;
 			plr.client.sendPacket(pk);
@@ -121,14 +121,14 @@ public class PlayAsNpcs implements ICenturiaModule {
 			Player plr = event.getAccount().getOnlinePlayerInstance();
 			ArrayList<String> removedItems = new ArrayList<String>();
 			ArrayList<JsonObject> removed = new ArrayList<JsonObject>();
-			JsonArray items = plr.account.getPlayerInventory().getItem("avatars").getAsJsonArray();
+			JsonArray items = plr.account.getSaveSpecificInventory().getItem("avatars").getAsJsonArray();
 			for (JsonElement ele : items) {
 				JsonObject avatar = ele.getAsJsonObject();
 				JsonObject info = avatar.get("components").getAsJsonObject().get("AvatarLook").getAsJsonObject()
 						.get("info").getAsJsonObject();
-				if (avatar.get("defId").getAsInt() == 8254) {
+				if (avatar.get("defId").getAsString().equals("8254")) {
 					// Kitsune slot
-					if (info.get("actorClassDefID").getAsInt() != 1929) {
+					if (!info.get("actorClassDefID").getAsString().equals("1929")) {
 						// Not a kitsune probably a npc so delete it
 						removed.add(avatar);
 						removedItems.add(avatar.get("id").getAsString());
@@ -142,7 +142,7 @@ public class PlayAsNpcs implements ICenturiaModule {
 			event.getAccount().getOnlinePlayerInstance().client.sendPacket(pkt);
 
 			// Save
-			event.getAccount().getPlayerInventory().setItem("avatars", items);
+			event.getAccount().getSaveSpecificInventory().setItem("avatars", items);
 			event.respond("Avatars updated");
 		}
 	}
